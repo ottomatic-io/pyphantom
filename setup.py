2146e27e97d5f3b479458cebca22407b5e6e3bea
Ben Hagen <c.ben.hagen@gmail.com>
2018-11-08 17:16:21 +0100

    Update metadata

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='pyphantom',
-      description='Phantom highspeed camera control',
-      url='http://gitlab.com/kamerawerk/pyphantom',
-      author='Ben Hagen',
-      author_email='ben@kamerawerk.ch',
-      install_requires=['psutil', 'PyYAML', 'netifaces', 'cached_property'],
-      packages=['pyphantom'],
-      scripts=[],
-      use_scm_version=True,
-      setup_requires=['pytest-runner', 'setuptools_scm'],
-      tests_require=['pytest', 'tempdir'],
-      )
+setup(
+    name="pyphantom",
+    description="Phantom® highspeed camera control",
+    url="http://gitlab.com/ottomatic/pyphantom",
+    author="Ben Hagen",
+    author_email="ben@ottomatic.io",
+    install_requires=["psutil", "PyYAML", "netifaces", "cached_property"],
+    packages=find_packages(),
+    scripts=[],
+    use_scm_version=True,
+    setup_requires=["pytest-runner", "setuptools_scm"],
+    tests_require=["pytest", "tempdir"],
+)
