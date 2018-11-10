85351d7b46ca988f26145bf65d3cc879e4c8fd63
Ben Hagen <c.ben.hagen@gmail.com>
2018-11-10 20:19:31 +0100

    Update setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name="pyphantom",
-    description="Phantom® highspeed camera control",
-    url="http://gitlab.com/ottomatic/pyphantom",
     author="Ben Hagen",
     author_email="ben@ottomatic.io",
+    description="Phantom® highspeed camera control",
+    entry_points={"console_scripts": ["pfs_cam = pyphantom.cli.pfs_cam:cli"]},
     install_requires=["psutil", "PyYAML", "netifaces", "cached_property", "click", "colorama", "timeout-decorator"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="pyphantom",
     packages=find_packages(),
     scripts=[],
-    entry_points={"console_scripts": ["pfs_cam = pyphantom.cli.pfs_cam:cli"]},
-    use_scm_version=True,
     setup_requires=["pytest-runner", "setuptools_scm"],
     tests_require=["pytest", "tempdir", "pcapy"],
+    url="http://gitlab.com/ottomatic/pyphantom",
+    use_scm_version=True,
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
 )
